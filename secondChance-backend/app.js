fece8264abc6f378a34b162f074fcec11f1fb0ce
Gergely Marton <wwworkingprocess@gmail.com>
2026-02-16 12:42:14 -0500

    implement registration

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // Route files
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
-//{{insert code here}}
+const authRoutes = require('./routes/authRoutes');
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
@@ -39,7 +39,7 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/auth', authRoutes);
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
---
+++ secondChance-backend/routes/authRoutes.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const pino = require('pino');  
+
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+dotenv.config();
+
+const logger = pino();  // Create a Pino logger instance
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+
+        // Task 3: Check if user credentials already exists in the database and throw an error if they do
+        const existingEmail = await collection.findOne({ email: req.body.email });
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
+
+        // Task 4: Create a hash to encrypt the password so that it is not readable in the database
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+
+        // Task 5: Insert the user into the database
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        // Task 6: Create JWT authentication if passwords match with user._id as payload
+        const payload = { user: { id: newUser.insertedId } };
+        const authtoken = jwt.sign(payload, JWT_SECRET); 
+
+        // Task 7: Log the successful registration using the logger
+        logger.info('User registered successfully');
+
+        // Task 8: Return the user email and the token as a JSON
+        return res.json({ authtoken, email: req.body.email });
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
+    }
+});
+
+module.exports = router;
